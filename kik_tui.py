a200bc34c173b84d5b49a89611f58bdbe33b5579
pttx <jacek.kusmierczyk@wp.pl>
2018-05-21 14:31:10 +0200

    ai

--- kik_tui.py
+++ kik_tui.py
@@ -1,5 +1,6 @@
 from kik_plansza import *
 from kik_kom import *
+from kik_ai_los import *
 
 
 def wyswietl_plansze(plansza):
@@ -11,18 +12,28 @@ def wyswietl_plansze(plansza):
         print("-------------------")
 
 
-def gra():
+## tryb 0 - człowiek vs człowiek
+## tryb 1 - człowiek vs komputer
+## tryb 2 - komputer vs komputer
+
+def partia(tryb,pierwszy_gracz):
 
     plansza = nowa_pusta_plansza()
     czy_ruch_wykonany = False
     czy_gra_skonczona = False
-    gracz = -1
+    gracz = pierwszy_gracz
     wygrany_gracz = 0
     while(not czy_gra_skonczona):
         wyswietl_plansze(plansza)
+        print(plansza)
         print(kom_ruch(gracz))
-        wiersz = int(input("podaj wiersz: "))
-        kolumna = int(input("podaj kolumne: "))
+        if(tryb == 0 or (tryb == 1 and gracz == pierwszy_gracz)):
+            wiersz = int(input("podaj wiersz: "))
+            kolumna = int(input("podaj kolumne: "))
+        else:
+            wiersz, kolumna = wybierz_ruch(plansza,gracz)
+            print(kom_ruch_przeciw(gracz, wiersz, kolumna))
+
         czy_ruch_wykonany, plansza, gracz = wykonaj_ruch(
             plansza, gracz, wiersz, kolumna)
 
@@ -31,13 +42,34 @@ def gra():
 
         print(plansza)
         czy_gra_skonczona, wygrany_gracz = koniec_gry(plansza)
-
+    
     print(kom_koniec(wygrany_gracz))
 
 
-graj = True
-while(graj):
-    gra()
-    x = input("Czy chcesz zagrać jeszcze raz? y/n: ")
-    if(x != "y" and x != "Y"):
-        graj = False
+def gra():
+    graj = True
+    print("tryb 0 - człowiek vs człowiek")
+    print("tryb 1 - człowiek vs komputer")
+    print("tryb 2 - komputer vs komputer")
+    tryb = int(input("Wybierz tryb (0,1,2): "))
+    print()
+    print("O: -1")
+    print("X: 1")
+    
+    gracz1 = int(input("czym chcesz zaczynać? -1/1: "))
+    
+    
+    while(graj):    
+        partia(tryb,pierwszy_gracz)
+        x = input("Czy chcesz zagrać jeszcze raz? y/n: ")
+        if(x != "y" and x != "Y"):
+            graj = False
+
+
+
+
+
+gra()
+
+
+
